55a5f198cb99a6d522d048dcf458a66d73970079
paul <paul@open.gov.sg>
2023-08-06 15:18:34 +0800

    updated summary

--- fe/src/components/Meals/MealsSummary.js
+++ fe/src/components/Meals/MealsSummary.js
@@ -1,19 +1,15 @@
-import classes from './MealsSummary.module.css';
+import classes from "./MealsSummary.module.css";
 
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Food, Delivered To You</h2>
+      <h2>Juicy Beans right at your doorstep!</h2>
+      <p>A Fruity Wonderland in your mouth.</p>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
-      </p>
-      <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        So breathe easy and enjoy the cool, refreshing sensation of a fresh
+        mouth!
       </p>
     </section>
   );
 };
-
 export default MealsSummary;
