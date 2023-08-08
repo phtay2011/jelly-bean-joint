651a0a26b1e52a5fc43d9c7f657a72ae8961db2a
paul <paul@open.gov.sg>
2023-08-08 13:31:32 +0800

    removed button

--- fe/src/components/Layout/Header.js
+++ fe/src/components/Layout/Header.js
@@ -1,22 +1,12 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
 // import mealsImage from "../../assets/meals.jpg";
 import background from "../../assets/Frame 42985.svg";
-import realImage from "../../assets/real-image.svg";
 import logo from "../../assets/logo.png";
 import classes from "./Header.module.css";
 
 const Header = (props) => {
-  const [useRealImage, setUseRealImage] = useState(false);
-
-  const revealImageHandler = () => {
-    if (useRealImage) {
-      setUseRealImage(false);
-    } else {
-      setUseRealImage(true);
-    }
-  };
   return (
     <Fragment>
       <header className={classes.header}>
@@ -25,16 +15,9 @@ const Header = (props) => {
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
-        {!useRealImage && (
-          <img src={background} alt="A table full of delicious food!" />
-        )}
-        {useRealImage && (
-          <img src={realImage} alt="A table full of delicious food!" />
-        )}
+        (
+        <img src={background} alt="A table full of delicious food!" />)
       </div>
-      <button onClick={revealImageHandler} className={classes.button}>
-        {" "}
-      </button>
     </Fragment>
   );
 };
